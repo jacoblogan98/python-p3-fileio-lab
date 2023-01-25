0b5d46e2fa15ed54e9f9e3a0808c07e3cc3e5efa
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-25 13:46:06 -0500

    passed tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode="w", encoding='utf-8') as file_write:
+        file_write.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode="a", encoding='utf-8') as file_append:
+        file_append.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode="r", encoding='utf-8') as file_read:
+        return file_read.read()
